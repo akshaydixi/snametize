df0c85ab8965523e677f11b49bfbb50aba81750a
Akshay Dixit <akshaydixi@gmail.com>
2015-04-04 23:04:52 +0530

    cleaned up .travis.yml

--- build/installCXXDependencies.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-echo "deb http://archive.ubuntu.com/ubuntu/ trusty main restricted" | sudo tee -a /etc/apt/sources.list
-sudo apt-get update -qq
-sudo apt-get install -qq libboost-program-options-dev gcc-4.8 g++-4.8 make cmake
