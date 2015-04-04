7a7cd84b0ecb667ffb96aa1ebbdb4593c2047d0b
Akshay Dixit <akshaydixi@gmail.com>
2015-04-04 22:37:08 +0530

    add apt-get-repository for travis v3

--- build/installCXXDependencies.sh
+++ build/installCXXDependencies.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
 
-sudo apt-get install software-properties-common python-software-properties
-sudo apt-get update
-sudo apt-get-repository "deb http://archive.ubuntu.com/ubuntu/ trusty main restricted" -y
+echo "deb http://archive.ubuntu.com/ubunut/ trusty main restricted" | sudo tee -a /etc/apt/sources.list
 sudo apt-get update -qq
 sudo apt-get install -qq libboost-program-options-dev g++-4.8 make cmake
