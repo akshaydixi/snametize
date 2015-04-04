ff857c43ba33fe90c9d0747bb03d664548726811
Akshay Dixit <akshaydixi@gmail.com>
2015-04-04 22:46:01 +0530

    fix for travis v5

--- build/installCXXDependencies.sh
+++ build/installCXXDependencies.sh
@@ -2,4 +2,4 @@
 
 echo "deb http://archive.ubuntu.com/ubuntu/ trusty main restricted" | sudo tee -a /etc/apt/sources.list
 sudo apt-get update -qq
-sudo apt-get install -qq libboost-program-options-dev g++-4.8 make cmake
+sudo apt-get install -qq libboost-program-options-dev gcc-4.8 g++-4.8 make cmake
