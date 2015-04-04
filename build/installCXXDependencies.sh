e6320e0786333ec7e0a6ce063c39a50072853026
Akshay Dixit <akshaydixi@gmail.com>
2015-04-04 22:30:36 +0530

    add apt-get-repository for travis v2

--- build/installCXXDependencies.sh
+++ build/installCXXDependencies.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 sudo apt-get install software-properties-common python-software-properties
+sudo apt-get update
 sudo apt-get-repository "deb http://archive.ubuntu.com/ubuntu/ trusty main restricted" -y
 sudo apt-get update -qq
-sudo apt-get install -qq libboost-program-options-dev gcc-4.8 make cmake
+sudo apt-get install -qq libboost-program-options-dev g++-4.8 make cmake
