2a001f3c2b63f76cbfe4cfc26685b6170105126c
Akshay Dixit <akshaydixi@gmail.com>
2015-04-04 21:39:55 +0530

    fix g++ for travis

--- build/installCXXDependencies.sh
+++ build/installCXXDependencies.sh
@@ -2,4 +2,4 @@
 
 sudo apt-get-repository "deb http://archive.ubuntu.com/ubuntu/ trusty main restricted" -y
 sudo apt-get update -qq
-sudo apt-get install -qq libboost-program-options-dev g++ make cmake
+sudo apt-get install -qq libboost-program-options-dev gcc-4.8 make cmake
