fd010fcccf3cc09cc434a186a1186006622b4c18
Akshay Dixit <akshaydixi@gmail.com>
2015-04-04 21:11:03 +0530

    integrated googletest and added a placeholder unit test

---
+++ test/small_webgraph.gml
@@ -0,0 +1,63 @@
+graph [
+directed 0
+vertex_properties [
+]
+edge_properties [
+]
+node [
+id 1
+]
+edge [
+id 1
+source 1
+target2
+]
+edge [
+id 2
+source 1
+target3
+]
+node [
+id 1
+]
+edge [
+id 3
+source 1
+target1
+]
+edge [
+id 4
+source 1
+target4
+]
+node [
+id 1
+]
+edge [
+id 5
+source 1
+target2
+]
+edge [
+id 6
+source 1
+target4
+]
+node [
+id 1
+]
+edge [
+id 7
+source 1
+target1
+]
+edge [
+id 8
+source 1
+target3
+]
+edge [
+id 9
+source 1
+target2
+]
---
+++ test/snametize_test.cc
@@ -0,0 +1,34 @@
+// Copyright 2015 Akshay Dixit
+#include <iostream>
+#include "gtest/gtest.h"
+#include "../src/snametize.h"
+
+const std::string SMALL_WEBGRAPH("./small_webgraph.txt");
+const std::string SMALLWEB_GRAPH_METIS("./small_webgraph.metis");
+const std::string SMALLWEB_GRAPH_SNAP("./small_webgraph.snap");
+const std::string SMALLWEB_GRAPH_GML("./small_webgraph.gml");
+
+class SnametizeSmallTest : public ::testing::Test {
+ protected:
+    const std::string tmpFilePath = "tmpFile";
+    SnametizeSmallTest() {
+    }
+
+    virtual ~SnametizeSmallTest() {
+    }
+
+    virtual void SetUp() {
+    }
+
+    virtual void TearDown() {
+       std::remove(tmpFilePath.c_str());
+    }
+};
+
+TEST_F(SnametizeSmallTest, TemporaryTest) {
+    int i = 1;
+    std::vector<uint64_t> v = {2, 3, 4};
+    std::ofstream tmpFile(tmpFilePath);
+    writeMetisLine(&tmpFile, v);
+    EXPECT_EQ(1, i);
+}
